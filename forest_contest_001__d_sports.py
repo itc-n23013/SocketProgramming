32a4fa373c2bf0b5844d80ed33646b2d4d0dd8bf
itc-n23013 <n23013@std.it-college.ac.jp>
2023-11-01 07:30:51 +0000

    1st

---
+++ forest_contest_001__d_sports.py
@@ -0,0 +1,9 @@
+s = input()
+sports_dict = {"baseball": 9,"soccer": 11,"rugby": 15,"basketball": 5,"volleyball": 6}
+
+if s in sports_dict:
+    result = sports_dict[s]
+else:
+    result
+
+print(result)
